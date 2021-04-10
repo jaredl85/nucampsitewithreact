59425a06b471301cf3bc3840a3afdd98588eb76b
Jared Loveridge <jaredloveridge@gmail.com>
2021-04-10 11:23:57 -0700

    week 2 assignment - begin

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const CampsiteInfo = this.props.
+        return (
+            if (campsite === true) {
+                return (
+                    <div className="row"></div>
+                );
+            } else {
+                return (
+                    <div></div>
+                );
+            }
+        );
+    }
+}
+
+export default CampsiteInfo;
